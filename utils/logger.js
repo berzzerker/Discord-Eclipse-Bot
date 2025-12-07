fcf9c565d7be0a2e8c2127337d4796d4bbfe7c9d
berzzerker <darkberzzerker@gmail.com>
2025-12-07 14:13:09 -0300

    fix 0.3

--- utils/logger.js
+++ utils/logger.js
@@ -2,13 +2,13 @@ const chalk = require('chalk');
 
 const logger = {
     info: (message) => {
-        console.log(chalk.blue(`[INFO] ${message}`));
+        console.log(chalk.default.blue(`[INFO] ${message}`));
     },
     warn: (message) => {
-        console.log(chalk.yellow(`[WARN] ${message}`));
+        console.log(chalk.default.yellow(`[WARN] ${message}`));
     },
     error: (message, error) => {
-        console.error(chalk.red(`[ERROR] ${message}`));
+        console.error(chalk.default.red(`[ERROR] ${message}`));
         if (error) {
             console.error(error);
         }
